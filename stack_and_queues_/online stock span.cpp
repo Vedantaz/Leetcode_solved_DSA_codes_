6783ca2a1c857a3bf1b116759e5daf32ab28aece
Vedantaz <muleyvedant4@gmail.com>
2023-07-11 20:18:37 +0530

    added stack imp questions else

---
+++ stack_and_queues_/online stock span.cpp
@@ -0,0 +1,20 @@
+ // count of less values at left
+ class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+
+        while(!st.empty() && st.top().first <= price){
+            span += st.top().second;
+            st.pop();
+
+        }
+        st.push({price, span});
+        return span;
+    }
+};
