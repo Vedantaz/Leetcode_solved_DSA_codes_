ba3d61eee752c16a4f75164ed0efa29f0afa3ad6
Vedantaz <muleyvedant4@gmail.com>
2024-09-07 15:20:30 +0530

    added and stared the tree codes in cpp

--- top 150 leetcode interview/tree/max depth.cpp
+++ top 150 leetcode interview/tree/max depth.cpp
@@ -44,7 +44,7 @@ public:
         if(root == nullptr){
             return 0;
         }
-        
+            
         int left = maxDepth(root->left);
         int right =maxDepth(root->right);
         
